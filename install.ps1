adbfe2303f531c037a6d9a927769e8dabb191148
Felipe Augusto <14238257+felipementel@users.noreply.github.com>
2022-10-05 08:57:01 -0300

    feat: add dotnet-coverage

--- install.ps1
+++ install.ps1
@@ -45,12 +45,13 @@ choco feature enable -n=allowGlobalConfirmation;
 . $PROFILE
 
 dotnet tool install --global dotnet-reportgenerator-globaltool
-dotnet tool install --global dotnet-trace
-dotnet tool install --global dotnet-sonarscanner
 dotnet tool install --global dotnet-aspnet-codegenerator
+dotnet tool install --global dotnet-coverage
+dotnet tool install --global dotnet-sonarscanner
+dotnet tool install --global dotnet-trace
 dotnet tool install --global dotnet-counters
-dotnet tool install --global dotnet-ef
 dotnet tool install --global dotnet-monitor
+dotnet tool install --global dotnet-ef
 
 <# excluded 
 ,"azure-functions-core-tools-4 --params "'/x64'""
