94da32a02b4e9d60f4677be5ada869f6657d9336
Felipe Augusto <14238257+felipementel@users.noreply.github.com>
2024-10-02 16:38:08 -0300

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,21 @@
+winget install --id CoreyButler.NVMforWindows --source winget ; `
+winget install --id Git.Git --source winget; `
+winget install --id GitHub.cli --source winget; `
+winget install --id Microsoft.Azure.StorageExplorer ; `
+winget install --id Microsoft.AzureCLI --source winget; `
+winget install --id k6.k6 --source winget; `
+winget install --id Microsoft.OpenJDK.21 --source winget; `
+winget install --id Microsoft.VisualStudioCode --source winget; `
+winget install --id Microsoft.VisualStudio.2022.Enterprise --source winget; `
+winget install --id CodecGuide.K-LiteCodecPack.Standard --source winget; `
+winget install --id 3TSoftwareLabs.Robo3T --source winget; `
+winget install --id Microsoft.DotNet.SDK.8 --source winget; `
+winget install --id Microsoft.DotNet.SDK.7 --source winget; `
+winget install --id Logitech.LogiTune --source winget; `
+winget install --id Mobatek.MobaXterm --source winget; `
+winget install --id JanDeDobbeleer.OhMyPosh --source winget; `
+winget install --id Postman.Postman --source winget; `
+winget install --id Zoom.Zoom.EXE --source winget; `
+winget install --id Microsoft.AzureDataStudio; `
+winget install --id JanDeDobbeleer.OhMyPosh --soruce winget; `
+winget install --id Telegram.TelegramDesktop --source winget
