060b77488419035f27e4b76578ac68a1e46c6d9d
Felipe Augusto <14238257+felipementel@users.noreply.github.com>
2024-11-13 14:15:18 -0300

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -10,7 +10,7 @@ winget install --id Microsoft.VisualStudio.2022.Enterprise --source winget; `
 winget install --id CodecGuide.K-LiteCodecPack.Standard --source winget; `
 winget install --id 3TSoftwareLabs.Robo3T --source winget; `
 winget install --id Microsoft.DotNet.SDK.8 --source winget; `
-winget install --id Microsoft.DotNet.SDK.7 --source winget; `
+winget install --id Microsoft.DotNet.SDK.9 --source winget; `
 winget install --id Logitech.LogiTune --source winget; `
 winget install --id Mobatek.MobaXterm --source winget; `
 winget install --id JanDeDobbeleer.OhMyPosh --source winget; `
