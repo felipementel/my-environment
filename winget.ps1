d58bc72e8dd5ad3ced72f9cdd3f28331da92983c
Felipe Augusto <14238257+felipementel@users.noreply.github.com>
2025-03-19 11:33:18 -0300

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,27 +1,32 @@
-winget install --id CoreyButler.NVMforWindows --source winget ; `
-winget install --id Git.Git --source winget; `
-winget install --id GitHub.cli --source winget; `
-winget install --id Microsoft.Azure.StorageExplorer ; `
-winget install --id Microsoft.AzureCLI --source winget; `
-winget install --id k6.k6 --source winget; `
-winget install --id Microsoft.OpenJDK.21 --source winget; `
-winget install --id Microsoft.VisualStudioCode --source winget; `
-winget install --id Microsoft.VisualStudio.2022.Enterprise --source winget; `
-winget install --id CodecGuide.K-LiteCodecPack.Standard --source winget; `
-winget install --id 3TSoftwareLabs.Robo3T --source winget; `
-winget install --id Microsoft.DotNet.SDK.8 --source winget; `
-winget install --id Microsoft.DotNet.SDK.9 --source winget; `
-winget install --id Logitech.LogiTune --source winget; `
-winget install --id Mobatek.MobaXterm --source winget; `
-winget install --id JanDeDobbeleer.OhMyPosh --source winget; `
-winget install --id Postman.Postman --source winget; `
-winget install --id Zoom.Zoom.EXE --source winget; `
-winget install --id Microsoft.AzureDataStudio; `
-winget install --id JanDeDobbeleer.OhMyPosh --soruce winget; `
-winget install --id Telegram.TelegramDesktop --source winget;
-winget install --id Insomnia.Insomnia --source winget;
-winget install --id DevToys-app.DevToys --source winget;
-winget install --id dbeaver.dbeaver --source winget;
+$packages = @(
+    "CoreyButler.NVMforWindows",
+    "Git.Git",
+    "GitHub.cli",
+    "Microsoft.Azure.StorageExplorer",
+    "Microsoft.AzureCLI",
+    "k6.k6",
+    "Microsoft.OpenJDK.21",
+    "Microsoft.VisualStudioCode",
+    "Microsoft.VisualStudio.2022.Enterprise",
+    "CodecGuide.K-LiteCodecPack.Standard",
+    "3TSoftwareLabs.Robo3T",
+    "Microsoft.DotNet.SDK.8",
+    "Microsoft.DotNet.SDK.9",
+    "Logitech.LogiTune",
+    "Mobatek.MobaXterm",
+    "JanDeDobbeleer.OhMyPosh",
+    "Postman.Postman",
+    "Zoom.Zoom.EXE",
+    "Microsoft.AzureDataStudio",
+    "Telegram.TelegramDesktop",
+    "Insomnia.Insomnia",
+    "DevToys-app.DevToys",
+    "dbeaver.dbeaver"
+)
+
+foreach ($package in $packages) {
+    winget install --id $package --source winget
+}
 
 & $PROFILE
 
