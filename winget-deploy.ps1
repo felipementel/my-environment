5282ff89f07a4467c66ec06a8dd24646f3bf0b77
Felipe Augusto <felipementel@hotmail.com>
2025-04-22 21:53:19 -0300

    feat: criando arquivo de winget para live

---
+++ winget-deploy.ps1
@@ -0,0 +1,43 @@
+$packages = @(
+    "CoreyButler.NVMforWindows",
+    "Git.Git",
+    "GitHub.cli",
+    "Microsoft.VisualStudioCode",
+    "3TSoftwareLabs.Robo3T",
+    "Microsoft.DotNet.SDK.9",
+    "Microsoft.Powershell"
+)
+
+foreach ($package in $packages) {
+    winget install --id $package --source winget
+}
+
+& $PROFILE
+
+$packagesDotNet = @(
+    "dotnet-reportgenerator-globaltool",
+    "dotnet-aspnet-codegenerator",
+    "dotnet-coverage",
+    "dotnet-sonarscanner",
+    "dotnet-trace",
+    "dotnet-counters",
+    "dotnet-monitor",
+    "dotnet-ef",
+    "Microsoft.dotnet-interactive"
+)
+
+foreach ($package in $packagesDotNet) {
+    dotnet tool install --global $package
+}
+
+& $PROFILE
+
+gh extension install github/gh-copilot
+
+& $PROFILE
+
+Update-Help
+
+& $PROFILE
+
+git config --global init.defaultBranch main
