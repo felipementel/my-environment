c794e651c6c34c79b46295964d887df7d9457a9c
Felipe Augusto <felipementel@hotmail.com>
2025-05-17 07:39:59 -0300

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,4 +1,4 @@
-Write-Host "Informações para configuração da conta Git"
+Write-Host "Informações para configuração da conta Git" -ForegroundColor Cyan
 
 $nome = Read-Host "Digite seu nome "
 $email = (Read-Host "Digite seu e-mail ").ToLower()
