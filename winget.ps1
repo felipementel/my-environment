47f4a17dd2a80b1cd1e677453de592b96872c0e7
Felipe Augusto <felipementel@hotmail.com>
2025-05-17 07:57:25 -0300

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,8 +1,8 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 Write-Host "Informações para configuração da conta Git" -ForegroundColor Cyan
 
-#$nome = Read-Host "Digite seu nome "
-#$email = (Read-Host "Digite seu e-mail ").ToLower()
+$nome = Read-Host "Digite seu nome "
+$email = (Read-Host "Digite seu e-mail ").ToLower()
 
 # Lista de pacotes a instalar via Winget
 $packages = @(
